fdd467719b4777e84968e22aae4fba50613a8b9b
GabbeKG <el_sueco91@hotmail.com>
2025-05-08 23:02:30 +0200

    achievement data source added

---
+++ backend/data/achievements.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    id: "great10",
+    name: "Great 10!",
+    description: "Taken in 10 or more interns at the same time",
+    points: 100,
+        badgeImage: "/badges/great10.png",
+    grade: "gold"
+  },
+  {
+    id: "pleaseStay",
+    name: "Please stay",
+    description: "Hired an intern",
+    points: 50,
+      badgeImage: "/badges/pleaseStay.png",
+    grade:"silver"
+  },
+  {
+    id: "welcome",
+    name: "Welcome!",
+    description: "Taken in an intern",
+    points: 25,
+      badgeImage: "/badges/welcome.png",
+    grade: "bronze"
+    
+  },
+  {
+    id: "twiceTheFun",
+    name: "Twice the fun!",
+    description: "Taken in 2 interns at the same time",
+    points: 50,
+      badgeImage: "/badges/twiceTheFun.png",
+    grade: "silver"
+  },
+  {
+    id: "gottaCatchEmAll",
+    name: "Gotta catch them all!",
+    description: "Hired 10 interns",
+    points: 150,
+      badgeImage: "/badges/gottaCatchEmAll.png"
+      ,
+    grade: "gold"
+  }
+];
